7beaf9d6471f8ddf3226e958c9194bb5d7572865
edgarburelo <edgar.burelo@gmail.com>
2023-08-17 15:03:48 +0200

    Resolve code analysis errors

--- src/Monkey/repl/repl.go
+++ src/Monkey/repl/repl.go
@@ -12,7 +12,10 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-	fmt.Fprintf(out, PROMPT)
+	_, err := fmt.Fprintf(out, PROMPT)
+	if err != nil {
+		return
+	}
 	scanned := scanner.Scan()
 	if !scanned {
 		return
@@ -22,6 +25,9 @@ func Start(in io.Reader, out io.Writer) {
 	l := lexer.New(line)
 
 	for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-		fmt.Fprintf(out, "%+v\n", tok)
+		_, err := fmt.Fprintf(out, "%+v\n", tok)
+		if err != nil {
+			return
+		}
 	}
 }
