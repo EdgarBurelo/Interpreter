1adcd064cbd8dc96ae285dcde4fe77a84024afa8
edgarburelo <edgar.burelo@gmail.com>
2023-09-03 11:39:17 +0200

    Parse return statements

--- src/Monkey/parser/parser_test.go
+++ src/Monkey/parser/parser_test.go
@@ -8,9 +8,9 @@ import (
 
 func TestLetStatements(t *testing.T) {
 	input := `
-let x 5;
-let = 10;
-let 838383;
+let x = 5;
+let y = 10;
+let foobar = 838383;
 `
 	l := lexer.New(input)
 	p := New(l)
@@ -36,6 +36,33 @@ let 838383;
 		stmt := program.Statements[i]
 		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
 			return
+		}
+	}
+}
+
+func TestReturnStatements(t *testing.T) {
+	input := `
+return 5;
+return 10;
+return 993322;
+`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+	if len(program.Statements) != 3 {
+		t.Fatalf("prgram.Statements deos no contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.ReturnStatement. got=%t", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returntStmt.Token Literal not 'return', got %q", returnStmt.TokenLiteral())
 		}
 	}
 }
